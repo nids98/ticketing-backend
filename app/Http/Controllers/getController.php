28e7c081625974e73e9f48bbf1067090d4515a42
Parth Patel <pmpp1920@Sahil.local>
2020-01-23 12:36:34 +0530

    added getController structure

---
+++ app/Http/Controllers/getController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class getController extends Controller
+{
+    public function getList(Request $request){
+
+        
+
+    }
+}
