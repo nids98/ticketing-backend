3de46c4a6049a2a510da3064eb7437d191b108a2
Parth Patel <pmpp1920@Sahil.local>
2020-01-23 15:34:56 +0530

    Edited getlist api-edited function and route

--- app/Http/Controllers/getController.php
+++ app/Http/Controllers/getController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class getController extends Controller
 {
-    public function getList(Request $request){
+    public function getList(Request $request,$tech_idd) {
 
-        
+        $array = DB::table('task')->select('task_id','subcategory_id','status','created_at')
+            ->where('tech_id', '=', $tech_idd)
+            ->get();
 
+
+        return response()->json($array);
     }
 }
